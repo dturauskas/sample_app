fcd1481ea7a76558e8f599522943f7c185d4aaf2
Darius Turauskas <darius.turauskas@gmail.com>
2012-05-08 12:51:23 +0300

    Finish user edit, update, index, and destroy actions

--- Gemfile
+++ Gemfile
@@ -3,10 +3,12 @@ source 'https://rubygems.org'
 gem 'rails', '3.2.3'
 gem 'bootstrap-sass', '2.0.0'
 gem 'bcrypt-ruby',  '3.0.1'
+gem 'faker',  '1.0.1'
+gem 'will_paginate', '3.0.3'
+gem 'bootstrap-will_paginate', '0.0.5'
 
-group :development, :test do
+group :development do
   gem 'sqlite3',  '1.3.5'
-  gem 'rspec-rails',  '2.9.0'
   gem 'annotate', '~> 2.4.1.beta'
 end  
 
@@ -20,7 +22,7 @@ end
 
 gem 'jquery-rails', '2.0.0'
 
-group :test do
+group :test, :development do
   gem 'rspec-rails',  '2.9.0'
   gem 'capybara', '1.1.2'
   gem 'factory_girl_rails', '1.4.0'
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,8 +16,10 @@ describe User do
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:authenticate) }
   it { should respond_to(:remember_token) }
+  it { should respond_to(:admin) }
   
   it { should be_valid }
+  it { should_not be_admin }
   
   describe "when name is not present" do
     before { @user.name = "" }
@@ -103,5 +105,21 @@ describe User do
   describe "remember token" do
     before { @user.save }
     its(:remember_token) { should_not be_blank }
+  end
+  
+  describe "with admin attribute set to 'true'" do
+    before { @user.toggle!(:admin) }
+    
+    it { should be_admin }
+  end
+  
+  describe "accessible attributes" do
+    it "should not allow access to admin" do
+      expect do
+        User.new(name: "Test User", email: "test@example.com",
+                 password: "foobar", password_confirmation: "foobar",
+                 admin: true)
+      end.should raise_error(ActiveModel::MassAssignmentSecurity::Error)
+    end
   end
 end
